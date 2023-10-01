853afba553e7bf8c0e8e2c07103d512c82b56b4e
DimaSudenkov <sudenkovdimas@gmail.com>
2023-10-01 18:01:30 +0300

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const container = document.querySelector(".gallery");
+console.log(container);
+
+container.addEventListener("click", handlerClick)
+container.insertAdjacentHTML("afterbegin", handlerModal(galleryItems))
+
+
+function handlerModal(arr) {
+  return arr.map(({ preview, original, description }) => `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+    </li>`
+  ).join('')
+}
+
+function handlerClick(evt) {
+  evt.preventDefault()
+  if (evt.target === evt.currentTarget) {
+    return
+  }
+  const lightbox = new SimpleLightbox('.gallery__item a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250
+  });
+}
+
